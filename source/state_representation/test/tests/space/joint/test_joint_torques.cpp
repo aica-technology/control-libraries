699ace4202a6f67ad204e74a67d73ccb67466cdf
Baptiste Busch <baptiste.busch@gmail.com>
2023-02-06 13:31:00 +0100

    refactor: correct operators in JointTorques

--- source/state_representation/test/tests/space/joint/test_joint_torques.cpp
+++ source/state_representation/test/tests/space/joint/test_joint_torques.cpp
@@ -138,11 +138,6 @@ TEST(JointTorquesTest, MatrixMultiplication) {
   JointTorques jscaled = gains * jt;
   EXPECT_EQ(jscaled.get_type(), StateType::JOINT_TORQUES);
   EXPECT_EQ(jscaled.data(), gains * jt.data());
-  EXPECT_EQ((jt * gains).data(), jscaled.data());
-  jt *= gains;
-  EXPECT_EQ(jt.get_type(), StateType::JOINT_TORQUES);
-  EXPECT_EQ(jscaled.data(), jt.data());
-  JointTorques jscaled2 = jt * gains;
 
   gains = Eigen::VectorXd::Random(2 * jt.get_size()).asDiagonal();
   EXPECT_THROW(gains * jt, exceptions::IncompatibleSizeException);
