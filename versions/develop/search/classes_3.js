6bbe541ece2092192eee0fe31f99ebb0570d33e1
domire8 <domire8@users.noreply.github.com>
2023-02-27 17:26:21 +0000

    deploy: 502c263cb3a63aad1153fd800072704bf9cb541d

--- versions/develop/search/classes_3.js
+++ versions/develop/search/classes_3.js
@@ -4,5 +4,6 @@ var searchData=
   ['emptyattractorexception_1',['EmptyAttractorException',['../classdynamical__systems_1_1exceptions_1_1_empty_attractor_exception.html',1,'dynamical_systems::exceptions']]],
   ['emptybaseframeexception_2',['EmptyBaseFrameException',['../classdynamical__systems_1_1exceptions_1_1_empty_base_frame_exception.html',1,'dynamical_systems::exceptions']]],
   ['emptystateexception_3',['EmptyStateException',['../classstate__representation_1_1exceptions_1_1_empty_state_exception.html',1,'state_representation::exceptions']]],
-  ['event_4',['Event',['../classstate__representation_1_1_event.html',1,'state_representation']]]
+  ['encodingexception_4',['EncodingException',['../class_encoding_exception.html',1,'']]],
+  ['event_5',['Event',['../classstate__representation_1_1_event.html',1,'state_representation']]]
 ];
