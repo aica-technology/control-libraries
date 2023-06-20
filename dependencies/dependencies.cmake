ae1337a3d24bd36fd463381602b178f873be2b32
Louis Brunner <louis.brunner.fr@gmail.com>
2023-06-20 17:56:47 +0100

    seemingly fine

---
+++ dependencies/dependencies.cmake
@@ -0,0 +1,23 @@
+# FIXME: it would be nicer to have it all in the root CMakelists.txt but:
+#  * `OVERRIDE_FIND_PACKAGE` requires CMake 3.24
+#  * some `protobuf` includes don't work when using `FetchContent`
+cmake_minimum_required(VERSION 3.18)
+project(control-libraries-dependencies)
+
+include(FetchContent)
+FetchContent_Declare(
+  OsqpEigen
+  GIT_REPOSITORY https://github.com/robotology/osqp-eigen.git
+  GIT_TAG        v0.6.4
+  OVERRIDE_FIND_PACKAGE
+)
+
+FetchContent_Declare(
+  protobuf
+  GIT_REPOSITORY https://github.com/protocolbuffers/protobuf.git
+  GIT_TAG        v3.17.0
+  SOURCE_SUBDIR  cmake
+  OVERRIDE_FIND_PACKAGE
+)
+
+FetchContent_MakeAvailable(OsqpEigen protobuf)
