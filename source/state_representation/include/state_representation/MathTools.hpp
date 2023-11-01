a70d204f85852abe34464ce9edc1211de1955683
Dominic Reber <dominic@aica.tech>
2023-11-01 07:31:07 +0000

    fix: remove unused function

--- source/state_representation/include/state_representation/MathTools.hpp
+++ source/state_representation/include/state_representation/MathTools.hpp
@@ -13,13 +13,6 @@ namespace state_representation::math_tools {
 const Eigen::Quaterniond log(const Eigen::Quaterniond& q);
 
 /**
- * @brief Calculate the exp of a quaternion as a non-unit quaternion
- * @param  q the quaternion to apply the exp on
- * @return the exp of the quaternion
- */
-const Eigen::Quaterniond exp(const Eigen::Quaterniond& q, double lambda = 1);
-
-/**
  * @brief Create a vector values from start to end
  * @param start the starting value
  * @param end the end value
