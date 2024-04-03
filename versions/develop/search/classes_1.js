836a1af36f4bc71b02cd3fe88ed59892c48ec576
yrh012 <yrh012@users.noreply.github.com>
2024-04-03 09:06:02 +0000

    deploy: 852f16400c8d688ef5e91142034e3b9c3cabe5e0

--- versions/develop/search/classes_1.js
+++ versions/develop/search/classes_1.js
@@ -6,6 +6,7 @@ var searchData=
   ['cartesiantwist_3',['CartesianTwist',['../classstate__representation_1_1_cartesian_twist.html',1,'state_representation']]],
   ['cartesianwrench_4',['CartesianWrench',['../classstate__representation_1_1_cartesian_wrench.html',1,'state_representation']]],
   ['circular_5',['Circular',['../classdynamical__systems_1_1_circular.html',1,'dynamical_systems']]],
-  ['complianttwist_6',['CompliantTwist',['../classcontrollers_1_1impedance_1_1_compliant_twist.html',1,'controllers::impedance']]],
-  ['controllerfactory_7',['ControllerFactory',['../classcontrollers_1_1_controller_factory.html',1,'controllers']]]
+  ['collisiongeometryexception_6',['CollisionGeometryException',['../classrobot__model_1_1exceptions_1_1_collision_geometry_exception.html',1,'robot_model::exceptions']]],
+  ['complianttwist_7',['CompliantTwist',['../classcontrollers_1_1impedance_1_1_compliant_twist.html',1,'controllers::impedance']]],
+  ['controllerfactory_8',['ControllerFactory',['../classcontrollers_1_1_controller_factory.html',1,'controllers']]]
 ];
