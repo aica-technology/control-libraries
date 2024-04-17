2b202834c8067500d73b48594d9e8496f208cba8
domire8 <domire8@users.noreply.github.com>
2024-04-17 10:30:40 +0000

    deploy: eddd9b77e975dc540569df1ab1e1b27a1a457b9d

---
+++ versions/v7.4.0/search/searchdata.js
@@ -0,0 +1,39 @@
+var indexSectionsWithContent =
+{
+  0: "abcdefgijlmnopqrstuvz~",
+  1: "acdefijmnpqrstv",
+  2: "cdrs",
+  3: "acdefgijlmnoprstuvz~",
+  4: "abcdfilnprsv",
+  5: "f",
+  6: "cdjmps",
+  7: "delos",
+  8: "bcdlmprs"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "namespaces",
+  3: "functions",
+  4: "variables",
+  5: "typedefs",
+  6: "enums",
+  7: "related",
+  8: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Classes",
+  2: "Namespaces",
+  3: "Functions",
+  4: "Variables",
+  5: "Typedefs",
+  6: "Enumerations",
+  7: "Friends",
+  8: "Pages"
+};
+
