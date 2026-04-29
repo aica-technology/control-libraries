e7cd3a2f6ee92d3e4b6e2917e871631c7899c0b7
domire8 <domire8@users.noreply.github.com>
2026-04-29 07:43:12 +0000

    deploy: 49563c5f071b5d6176430a214611e72a743c0d4b

--- versions/rolling/search/classes_6.js
+++ versions/rolling/search/classes_6.js
@@ -6,6 +6,7 @@ var searchData=
   ['jointpositions_3',['JointPositions',['../classstate__representation_1_1_joint_positions.html',1,'state_representation']]],
   ['jointstate_4',['JointState',['../classstate__representation_1_1_joint_state.html',1,'state_representation']]],
   ['jointtorques_5',['JointTorques',['../classstate__representation_1_1_joint_torques.html',1,'state_representation']]],
-  ['jointvelocities_6',['JointVelocities',['../classstate__representation_1_1_joint_velocities.html',1,'state_representation']]],
-  ['jsonparsingexception_7',['JsonParsingException',['../classclproto_1_1_json_parsing_exception.html',1,'clproto']]]
+  ['jointtypevisitor_6',['JointTypeVisitor',['../structrobot__model_1_1_joint_type_visitor.html',1,'robot_model']]],
+  ['jointvelocities_7',['JointVelocities',['../classstate__representation_1_1_joint_velocities.html',1,'state_representation']]],
+  ['jsonparsingexception_8',['JsonParsingException',['../classclproto_1_1_json_parsing_exception.html',1,'clproto']]]
 ];
